433015ec76ae29b199fe992ff00516822a354a19
Nishimura221 <huy25948@gmail.com>
2025-09-30 22:38:19 +0700

    feat(vehicle): create the VehicleRepo and IVehicleRepo add the controller

---
+++ Repositories/Repositories/VehicleRepo/IVehicleRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects.Enums;
+using BusinessObjects.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories.Repositories.VehicleRepo
+{
+    public interface IVehicleRepo
+    {
+        Task<Vehicle> GetVehicleById(string vehicleId);
+        Task<List<Vehicle>> GetAllVehicles();
+        Task<Vehicle> AddVehicle(Vehicle vehicle);
+        Task<Vehicle> UpdateVehicle(Vehicle vehicle);
+
+    }
+}
