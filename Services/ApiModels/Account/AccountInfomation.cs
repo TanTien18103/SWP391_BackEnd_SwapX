a2bfe02d4ada0c726fe78c7f9b4597d166898ce6
Nishimura221 <huy25948@gmail.com>
2025-10-08 07:20:18 +0700

    fix(account): add more field to show more detail

--- Services/ApiModels/Account/AccountInfomation.cs
+++ Services/ApiModels/Account/AccountInfomation.cs
@@ -2,15 +2,14 @@ namespace Services.ApiModels;
 
 public class AccountInfomation
 {
+    public string AccountId { get; set; }
     public string Username { get; set; }
-
     public string Name { get; set; }
-
     public string Email { get; set; }
-
     public string Status { get; set; }
-
+    public string Phone { get; set; }
+    public string role { get; set; }
+    public string Address { get; set; }
     public DateTime? StartDate { get; set; }
-
     public DateTime? UpdateDate { get; set; }
 }
